ea66edc39be782abbf5e9703d33993fbffb5fb8d
HodaFakharzadeh <36373856+HodaFakharzadeh@users.noreply.github.com>
2018-02-11 22:54:11 +0330

    Update Main.java

--- Hello world/Main.java
+++ Hello world/Main.java
@@ -1,5 +1,9 @@
-package tehran;
+package javaLaitec;
 
 public class Main {
 
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hi Donya!");
+    }
 }
